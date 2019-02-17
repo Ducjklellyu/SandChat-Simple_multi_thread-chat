a12f2ed1ec02e5ac6a2b10c9bb1e4ee0d33ef6e3
ducjklellyu <43794486+ducjklellyu@users.noreply.github.com>
2019-02-17 23:19:42 +0300

    Update sandchat_common.h

--- Client/sandchat_common.h
+++ Client/sandchat_common.h
@@ -1,6 +1,6 @@
 /*
 (SandChat(Simple multi-thread chat))::(Client)::(sandchat_common.h).
-Copyright © 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
+Copyright Â© 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
 
 GitHub profile - "https://github.com/ducjklellyu".
 Telegram - "@Sunuia".
@@ -9,3 +9,4 @@ Telegram - "@Sunuia".
 #pragma once
 #define  _FUNCTHREAD unsigned int __stdcall 
 #define	 _CLEARBUFF(x) (x=0)
+#define  _GET(x) (gets(x))
