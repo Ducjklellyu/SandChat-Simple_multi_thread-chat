b44134ead0fd1a2b75cdbdc74220c61d28272433
ducjklellyu <43794486+ducjklellyu@users.noreply.github.com>
2019-03-26 19:34:45 +0300

    Update stringbase.h

--- Client/stringbase.h
+++ Client/stringbase.h
@@ -1,6 +1,5 @@
 /*
 (SandChat(Simple multi-thread chat))::(Client)::(stringbase.h)
-Ñopyright © 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
 
 GitHub profile - "https://github.com/ducjklellyu".
 Telegram - "@Sunuia".
