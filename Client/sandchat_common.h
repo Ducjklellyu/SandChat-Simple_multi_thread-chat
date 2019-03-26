6844618bd6dd2f756376587762d017b3507c947b
ducjklellyu <43794486+ducjklellyu@users.noreply.github.com>
2019-03-26 19:35:37 +0300

    Update sandchat_common.h

--- Client/sandchat_common.h
+++ Client/sandchat_common.h
@@ -1,6 +1,5 @@
 /*
 (SandChat(Simple multi-thread chat))::(Client)::(sandchat_common.h).
-Copyright © 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
 
 GitHub profile - "https://github.com/ducjklellyu".
 Telegram - "@Sunuia".
