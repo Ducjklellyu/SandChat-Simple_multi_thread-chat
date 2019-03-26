4c67090cb8b41655724400267dff4e23e5999444
ducjklellyu <43794486+ducjklellyu@users.noreply.github.com>
2019-03-26 19:32:44 +0300

    Update clientinit.h

--- Client/clientinit.h
+++ Client/clientinit.h
@@ -1,6 +1,6 @@
 /*
 (SandChat(Simple multi-thread chat))::(Client)::(clientinit.h)
-Ñopyright © 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
+Ã‘opyright Â© 2017 by ducjklellyu (a.k.a Alexander Rosenthal) all rights reserved.
 
 GitHub profile - "https://github.com/ducjklellyu".
 Telegram - "@Sunuia".
@@ -17,6 +17,9 @@ Telegram - "@Sunuia".
 #include <stdio.h>
 #include <stdlib.h>
 
-void start_WSA(WSADATA WSAdata); // Initialize WINSOCK library function.
-void ex_SOCKET(SOCKET Socket); // Create SOCKET function.
-void ex_CONNECT(SOCKET Socket, SOCKADDR_IN addr); // Connect to server function.
+// Initialize WINSOCK library function.
+void start_WSA(WSADATA WSAdata); 
+// Create SOCKET function.
+void ex_SOCKET(SOCKET Socket); 
+// Connect to server function.
+void ex_CONNECT(SOCKET Socket, SOCKADDR_IN addr); 
